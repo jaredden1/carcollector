65658b191180fbfc3c4b6446e07c94b2d7131d92
jaredden <james.ifit@gmail.com>
2023-09-20 15:20:06 -0400

    added cars path

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+cars = [
+  {'make': 'Toyota', 'model': 'Supra', 'year': 1998},
+  {'make': 'Genesis', 'model': 'Coupe 380GT', 'year': 2010},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cars_idx(request):
+    return render(request, 'cars/idx.html', {
+        'cars': cars
+    })
