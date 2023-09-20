19c7e84e44b3228311494abc942cebe1d2e4355c
jaredden <james.ifit@gmail.com>
2023-09-20 18:58:03 -0400

    updated idx.html and fixed year in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    year = models.CharField(max_length=100)
+    year = models.IntegerField()
     description = models.TextField(max_length=1000)
 
     def __str__(self):
